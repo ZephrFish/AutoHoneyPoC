737e4543682ace69277cda9dfb1eaa5deeab1f92
ZephrFish <ZephrFish@users.noreply.github.com>
2021-06-16 23:47:27 +0100

    Update compileGo.py

--- src/functions/compileGo.py
+++ src/functions/compileGo.py
@@ -7,7 +7,7 @@ def compile_go(path, cve):
 		# print(path)
 		# print(cve)
 		# Line below is a fix for WSL
-		os.system(f'env GOOS=windows GOARCH=amd64 go build -o {path}{cve}.exe {path}{cve}.go')
+		os.system(f'env GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o {path}{cve}.exe {path}{cve}.go')
 		# Line below works on linux by default (kali, ubuntu)
 		# os.system(f'env GOOS=windows GOARCH=amd64 go build -o {path} {path}{cve}.go')
 		os.system(f'rm {path}{cve}.go')
