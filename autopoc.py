2d7f1a8b2e45e31e4b5ed732e4a6f8b33f400cdf
ZephrFish <ZephrFish@users.noreply.github.com>
2021-08-27 19:29:07 +0100

    Update autopoc.py

--- autopoc.py
+++ autopoc.py
@@ -44,6 +44,16 @@ from src.template_edit_functions.replaceInformation import replaceInformation
 from src.template_edit_functions.open_close_write_Template import open_template, close_template, write_template
 from src.template_edit_functions.write_Readme import write_readme
 
+# Colours
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk))
+
 '''
 # Global Variables
 ~ Description : These are here because they sit better here then anywhere else in the code multiple places use them,
