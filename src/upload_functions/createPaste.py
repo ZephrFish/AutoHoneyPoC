5e59f92dfa269995e1ad642f86628240e00827b8
Andy Gill <ZephrFish@users.noreply.github.com>
2024-04-26 13:45:40 +0100

    spring cleaning

--- src/upload_functions/createPaste.py
+++ src/upload_functions/createPaste.py
@@ -1,13 +1,14 @@
-import json
+
 import requests
+
 
 # Stage 2b: Create Pastebin Entry with git repo name and CVE information
 def createPaste(cve, GITHUB_USERNAME, PASTE_API_KEY):
-	RepoID = f'https://github.com/{GITHUB_USERNAME}/{cve}.git'
-	data = {
-		'api_dev_key': PASTE_API_KEY,
-		'api_paste_code': f'New PoC Published for {cve} located at {RepoID} ',
-		'api_option': 'paste'
-		}
-	r=requests.post('https://pastebin.com/api/api_post.php', data=data)
-	print(r.content)
+    RepoID = f"https://github.com/{GITHUB_USERNAME}/{cve}.git"
+    data = {
+        "api_dev_key": PASTE_API_KEY,
+        "api_paste_code": f"New PoC Published for {cve} located at {RepoID} ",
+        "api_option": "paste",
+    }
+    r = requests.post("https://pastebin.com/api/api_post.php", data=data)
+    print(r.content)
